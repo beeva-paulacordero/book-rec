f093cebf39b7a155e565b07bb300c1ab778dd826
Paula Cordero <paula.cordero@beeva.com>
2014-12-03 13:50:47 +0100

    Correccion query optimizada en todos los filtros

--- consultasServer.js
+++ consultasServer.js
@@ -18,15 +18,8 @@ ModelKnowledgeGraph.prototype.consultaColeccionSinFiltro= function(libros, limit
 
 	var deferred = Q.defer();
 
-		console.log("ESTOY A PUNTO DE LANZAR LA CONSULTA");
-
-		arrayquery = "MATCH (l:Libro)<-[r:IND1_JACCARD]->(v:Libro) \n"
-		+ "WHERE" + libros + " AND NOT (l.codigo_libro=v.codigo_libro) \n"
-		+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
-//		+ "ORDER BY indice DESC \n"
-		+ "LIMIT "+ limit +" \n"
-
 console.log(libros);
+
 //libros = '"2900000014271","2900000089091"';
 arrayquery = "START aa=node:Libro('codigo_libro:("+libros+")')\n" 
 	+ "MATCH aa<-[r:IND1_JACCARD]->(v:Libro)\n"
@@ -34,6 +27,7 @@ arrayquery = "START aa=node:Libro('codigo_libro:("+libros+")')\n"
 	+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
 	+ "ORDER BY indice DESC\n"
 	+ "LIMIT " + limit + "\n";
+
 		console.log(arrayquery);
 
 		dbneo.query(arrayquery, function(err, information){
@@ -54,11 +48,12 @@ ModelKnowledgeGraph.prototype.consultaColeccionConFiltros= function(libros, limi
 
 	var deferred = Q.defer();
 		
-		arrayquery = "MATCH (l:Libro)<-[r:IND1_JACCARD]->(v:Libro) \n"
-		+ "WHERE" + libros + " AND (l.tipo=v.tipo) AND (l.familia=v.familia) AND (l.subfamilia=v.subfamilia) AND NOT (l.codigo_libro=v.codigo_libro) \n"
-		+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
-		+ "ORDER BY indice DESC \n"
-		+ "LIMIT "+ limit +" \n"
+		arrayquery = "START aa=node:Libro('codigo_libro:("+libros+")')\n" 
+	+ "MATCH aa<-[r:IND1_JACCARD]->(v:Libro)\n"
+	+ "WHERE NOT v.codigo_libro IN ["+ libros + "] AND (aa.tipo=v.tipo) AND (aa.familia=v.familia) AND (aa.subfamilia=v.subfamilia)\n"
+	+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
+	+ "ORDER BY indice DESC\n"
+	+ "LIMIT " + limit + "\n";
 
 		console.log(arrayquery);
 
@@ -80,11 +75,12 @@ ModelKnowledgeGraph.prototype.consultaColeccionConFiltroTipo= function(libros, l
 
 	var deferred = Q.defer();
 
-		arrayquery = "MATCH (l:Libro)<-[r:IND1_JACCARD]->(v:Libro) \n"
-		+ "WHERE" + libros + " AND (l.tipo=v.tipo) AND NOT (l.codigo_libro=v.codigo_libro) \n"
-		+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
-		+ "ORDER BY indice DESC \n"
-		+ "LIMIT "+ limit +" \n"
+		arrayquery = "START aa=node:Libro('codigo_libro:("+libros+")')\n" 
+	+ "MATCH aa<-[r:IND1_JACCARD]->(v:Libro)\n"
+	+ "WHERE NOT v.codigo_libro IN ["+ libros + "] AND (aa.tipo=v.tipo)\n"
+	+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
+	+ "ORDER BY indice DESC\n"
+	+ "LIMIT " + limit + "\n";
 
 		console.log(arrayquery);
 
@@ -106,11 +102,12 @@ ModelKnowledgeGraph.prototype.consultaColeccionConFiltroFamilia= function(libros
 
 	var deferred = Q.defer();
 
-		arrayquery = "MATCH (l:Libro)<-[r:IND1_JACCARD]->(v:Libro) \n"
-		+ "WHERE" + libros + " AND (l.familia=v.familia) AND (l.subfamilia=v.subfamilia) AND NOT (l.codigo_libro=v.codigo_libro) \n"
-		+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
-		+ "ORDER BY indice DESC \n"
-		+ "LIMIT "+ limit +" \n"
+		arrayquery = "START aa=node:Libro('codigo_libro:("+libros+")')\n" 
+	+ "MATCH aa<-[r:IND1_JACCARD]->(v:Libro)\n"
+	+ "WHERE NOT v.codigo_libro IN ["+ libros + "] AND (aa.familia=v.familia) AND (aa.subfamilia=v.subfamilia) \n"
+	+ "RETURN SUM(r.valor) AS indice, v.codigo_libro AS id, v.nombre AS t \n"
+	+ "ORDER BY indice DESC\n"
+	+ "LIMIT " + limit + "\n";
 
 		console.log(arrayquery);
 
@@ -126,132 +123,6 @@ ModelKnowledgeGraph.prototype.consultaColeccionConFiltroFamilia= function(libros
 			}
 		});	 
 		return deferred.promise;
-}
-
-// RECOMENDADOR SOBRE UN ÚNICO LIBRO
-
-ModelKnowledgeGraph.prototype.consultarNodo= function(libro, res){
-
-	var deferred = Q.defer();
-
-	arrayquery = "MATCH (n:Libro {codigo_libro: '"+ libro +"'}) \n"
-	+ "RETURN n.nombre AS nombre"
-
-	dbneo.query(arrayquery, function(err, information){
-
-		if(err){
-			deferred.reject(new Error(err));
-
-		} else{
-			
-			deferred.resolve(information);
-			console.log('\nOk! he consultado el NODO \n' + JSON.stringify(information)); 
-		}
-	});	 
-	return deferred.promise;
-}
-
-ModelKnowledgeGraph.prototype.consultaRecomendadorSinFiltro= function(libro, res){
-
-	var deferred = Q.defer();
-
-	arrayquery = "MATCH (n:Libro {codigo_libro: '"+ libro +"'}) <-[r:IND1_JACCARD]->(l:Libro) \n"
-	+ "RETURN l.nombre AS nombre\n" 
-	+ "ORDER BY r.valor DESC \n"
-	+ "LIMIT 3 \n"
-
-	console.log(arrayquery);
-
-	dbneo.query(arrayquery, function(err, information){
-
-		if(err){
-			deferred.reject(new Error(err));
-
-		} else{
-			
-			deferred.resolve(information);
-			console.log('\nOk! he consultado el NODO \n' + JSON.stringify(information)); 
-		}
-	});	 
-	return deferred.promise;
-}
-
-ModelKnowledgeGraph.prototype.consultaRecomendadorConFiltros= function(libro, res){
-
-	var deferred = Q.defer();
-
-	arrayquery = "MATCH (n:Libro {codigo_libro: '"+ libro +"'}) <-[r:IND1_JACCARD]->(l:Libro) \n"
-	+ "WHERE n.tipo=l.tipo AND n.familia=l.familia AND n.subfamilia=l.subfamilia \n"
-	+ "RETURN l.nombre AS nombre \n" 
-	+ "ORDER BY r.valor DESC \n"
-	+ "LIMIT 3 \n"
-
-	console.log(arrayquery);
-
-	dbneo.query(arrayquery, function(err, information){
-
-		if(err){
-			deferred.reject(new Error(err));
-
-		} else{
-			
-			deferred.resolve(information);
-			console.log('\nOk! he consultado el NODO \n' + JSON.stringify(information)); 
-		}
-	});	 
-	return deferred.promise;
-}
-
-ModelKnowledgeGraph.prototype.consultaRecomendadorConFiltroTipo= function(libro, res){
-
-	var deferred = Q.defer();
-
-	arrayquery = "MATCH (n:Libro {codigo_libro: '"+ libro +"'}) <-[r:IND1_JACCARD]->(l:Libro) \n"
-	+ "WHERE n.tipo=l.tipo \n"
-	+ "RETURN l.nombre AS nombre \n" 
-	+ "ORDER BY r.valor DESC \n"
-	+ "LIMIT 3 \n"
-
-	console.log(arrayquery);
-
-	dbneo.query(arrayquery, function(err, information){
-
-		if(err){
-			deferred.reject(new Error(err));
-
-		} else{
-			
-			deferred.resolve(information);
-			console.log('\nOk! he consultado el NODO \n' + JSON.stringify(information)); 
-		}
-	});	 
-	return deferred.promise;
-}
-
-ModelKnowledgeGraph.prototype.consultaRecomendadorConFiltroFamilia= function(libro, res){
-
-	var deferred = Q.defer();
-
-	arrayquery = "MATCH (n:Libro {codigo_libro: '"+ libro +"'}) <-[r:IND1_JACCARD]->(l:Libro) \n"
-	+ "WHERE n.familia=l.familia AND n.subfamilia=l.subfamilia \n"
-	+ "RETURN l.nombre AS nombre \n" 
-	+ "ORDER BY r.valor DESC \n"
-	+ "LIMIT 3 \n"
-
-	console.log(arrayquery);
-
-	dbneo.query(arrayquery, function(err, information){
-
-		if(err){
-			deferred.reject(new Error(err));
-
-		} else{
-			
-			deferred.resolve(information);
-			console.log('\nOk! he consultado el NODO \n' + JSON.stringify(information)); 
-		}
-	});	 
-	return deferred.promise;
 }
 
 module.exports = ModelKnowledgeGraph;
